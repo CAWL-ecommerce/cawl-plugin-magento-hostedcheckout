a317a482e7b10f36ceb16d9c361ddc97313f0568
CAWL ecommerce <ecommerce-integrations@cawl.com>
2025-07-11 15:57:17 +0200

    fix issue with CVCO when using manual capture

--- Plugin/Magento/Payment/Model/Method/Adapter/ReplacePaymentAction.php
+++ Plugin/Magento/Payment/Model/Method/Adapter/ReplacePaymentAction.php
@@ -6,9 +6,12 @@ namespace Cawl\HostedCheckout\Plugin\Magento\Payment\Model\Method\Adapter;
 use Magento\Payment\Model\Method\Adapter;
 use Cawl\HostedCheckout\Model\Config\PaymentActionReplaceHandlerInterface;
 use Cawl\HostedCheckout\Model\Data\OrderPaymentContainer;
+use Cawl\HostedCheckout\Gateway\Config\Config;
 
 class ReplacePaymentAction
 {
+    public const WORLD_LINE_CHEQUE_VACANCES_ONLINE_METHOD = 'worldline_redirect_payment_5403';
+
     /**
      * @var PaymentActionReplaceHandlerInterface[]
      */
@@ -35,6 +38,10 @@ class ReplacePaymentAction
      */
     public function afterGetConfigPaymentAction(Adapter $subject, ?string $result = null): ?string
     {
+        if ($subject->getCode() === self::WORLD_LINE_CHEQUE_VACANCES_ONLINE_METHOD) {
+            return Config::AUTHORIZE_CAPTURE;
+        }
+
         if (!$payment = $this->orderPaymentContainer->getPayment()) {
             return $result;
         }
